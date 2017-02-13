8ee5829fe94cfbf0c7abaf072d55646519e4bfbd
pohsun.lu <pohsun.lu@nogle.com>
2017-02-13 16:27:57 +0800

    temporary commit

--- js/containers/root/rootContainer.js
+++ js/containers/root/rootContainer.js
@@ -3,6 +3,8 @@
 import { connect } from 'react-redux';
 import Navigation from '../../components/navigationView/navigationView';
 import { changeTab } from '../../actions/tabActions';
+import * as PRESS from '../../constants/constants';
+// import { PRESS_FEED, PRESS_HEATMAP, PRESS_CURRICULUM, PRESS_ACTIVITY, PRESS_PROFILE } from '../constants/constants';
 
 function mapDispatchToProps( dispatch : Function ) {
     return {
@@ -16,8 +18,8 @@ function mapDispatchToProps( dispatch : Function ) {
 // }
 
 function mapStateToProps( state ) {
-    const index = state.tabState.indexOfTabs;
-    return { index: index, title: state.tabState.tabs[index].title };
+    const index = state.tabState.tabState.indexOfTabs;
+    return { index: index, title: state.tabState.tabState.tabs[index].title };
 }
 
 module.exports = connect( mapStateToProps, mapDispatchToProps )( Navigation );
